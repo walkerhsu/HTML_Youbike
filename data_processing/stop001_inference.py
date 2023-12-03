1d232a6a32c0140f8efd6bb0112872e109ae203b
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2023-12-03 23:25:22 +0800

    :sparkle: add RNN model

---
+++ data_processing/stop001_inference.py
@@ -0,0 +1,20 @@
+import csv
+
+data = []
+info = []
+with open('inference_csv/inf_1204_to_1210.csv', 'r') as f:
+    reader = csv.reader(f)
+    i = 0
+    for row in reader:
+        if i != 0:
+            data.append(row)
+        else:
+            info.append(row)
+        i += 1
+# print(data)    
+data = list(filter(lambda x: x[5] == "25.02605", data))
+# print(data)
+data = info + data
+with open('stop001_inf_1204_to_1210.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
